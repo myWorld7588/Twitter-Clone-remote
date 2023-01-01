10aef934cbb57a20491384a0698fac1bfd96b54b
jakechoi <jin.jakechoi@gmail.com>
2023-01-01 17:15:56 -0500

    create CreateTweetView & multiline text field

---
+++ Twitter-Clone/View/Post/CreateTweetView.swift
@@ -0,0 +1,49 @@
+//
+//  CreateTweetView.swift
+//  Twitter-Clone
+//
+//  Created by Jake Choi on 1/1/23.
+//
+
+import SwiftUI
+
+struct CreateTweetView: View {
+    @State var text = ""
+
+    var body: some View {
+        
+        VStack {
+            HStack {
+                // MARK: - Cancel
+                Button(action: {
+                    // action
+                }, label: {
+                    Text("Cancel")
+                })
+                
+                Spacer()
+                
+                // MARK: - Tweet
+                Button(action: {
+                    // action
+                }, label: {
+                    Text("Tweet")
+                        .padding()
+                })
+                .background(Color("bg"))
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            } //: HSTACK
+            
+            MultilineTextfield(text: $text)
+            
+        } //: VSTACK
+        .padding()
+    }
+}
+
+struct CreateTweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateTweetView()
+    }
+}
