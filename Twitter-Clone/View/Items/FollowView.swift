7b6ebeea9fef4f6dcb6f91b1d40c45f00328c042
jakechoi <jin.jakechoi@gmail.com>
2023-01-02 20:00:07 -0500

    create reusable Follow Me create toggle() chevron up and down

--- Twitter-Clone/View/Items/FollowView.swift
+++ Twitter-Clone/View/Items/FollowView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct FollowView: View {
+    
+    var count: Int
+    var title: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct FollowView_Previews: PreviewProvider {
-    static var previews: some View {
-        FollowView()
+        HStack {
+            Text("\(count)")
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+            
+            Text(title)
+                .foregroundColor(.gray)
+        }
     }
 }
--- Twitter-Clone/View/Main/SlideMenu.swift
+++ Twitter-Clone/View/Main/SlideMenu.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct SlideMenu: View {
+    // toggle chevron
+    @State var show = false
+    
     var body: some View {
         VStack {
             HStack(spacing: 0, content: {
@@ -29,7 +32,27 @@ struct SlideMenu: View {
                             
                             Text("@cem_salta")
                                 .foregroundColor(.gray)
+                            
+                            HStack(spacing: 20, content: {
+                                FollowView(count: 0, title: "Following")
+                                FollowView(count: 16, title: "Followers")
+                            })
+                            .padding(.top, 10)
+                            
+                            Divider()
+                                .padding(.top, 10)
+                            
                         }) //: VSTACK
+                        
+                        // chevron button
+                        Button(action: {
+                            withAnimation{
+                                self.show.toggle()
+                            }
+                        }, label: {
+                            Image(systemName: show ? "chevron.down" : "chevron.up")
+                                .foregroundColor(Color("bg"))
+                        })
                     }) //: HSTACK
                     
                 }) //: VSTACK
