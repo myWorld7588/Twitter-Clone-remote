7430751eed0eadfe15a6e314777e1b5960fa7c40
jakechoi <jin.jakechoi@gmail.com>
2023-01-03 23:39:57 -0500

    .onTapgesture on SlideMenu x = -width

--- Twitter-Clone/View/Main/MainView.swift
+++ Twitter-Clone/View/Main/MainView.swift
@@ -25,7 +25,13 @@ struct MainView: View {
                     SlideMenu()
                         .shadow(color: Color.black.opacity(x != 0 ? 0.1 : 0), radius: 5, x: 5, y: 0)
                         .offset(x: x)
-                        
+                        .background(Color.black.opacity(x == 0 ? 0.5 : 0))
+                        .ignoresSafeArea(.all, edges: .vertical)
+                        .onTapGesture {
+                            withAnimation {
+                                x = -width
+                            }
+                        }
                 })
                 
             }
