db11d3e50017080d38dfde8511eddc0585377ba2
jakechoi <jin.jakechoi@gmail.com>
2023-01-04 20:36:53 -0500

    First commit User Profile

---
+++ Twitter-Clone/View/Profile/UserProfile.swift
@@ -0,0 +1,22 @@
+//
+//  UserProfile.swift
+//  Twitter-Clone
+//
+//  Created by Jake Choi on 1/4/23.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            
+        }
+    }
+}
+
+struct UserProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfile()
+    }
+}
