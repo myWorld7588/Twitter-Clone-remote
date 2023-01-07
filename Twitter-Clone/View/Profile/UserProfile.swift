d595d3640a47d7dc2f6b3ec07bb131bb331b13c9
jakechoi <jin.jakechoi@gmail.com>
2023-01-07 00:03:49 -0500

    scale effect when scrolls and create button

--- Twitter-Clone/View/Profile/UserProfile.swift
+++ Twitter-Clone/View/Profile/UserProfile.swift
@@ -56,6 +56,37 @@ struct UserProfile: View {
                 }
                 .frame(height: 180)
                 .zIndex(1)
+                
+                
+                VStack {
+                    HStack {
+                        Image("logo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 75, height: 75)
+                            .clipShape(Circle())
+                            .padding(8)
+                            .background(Color.white)
+                            .offset(y: offset < 0 ? getOffset() - 20 : -20)
+                            .scaleEffect(getScale())
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Edit Profile")
+                                .foregroundColor(.blue)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal)
+                                .background(Capsule().stroke(Color.blue, lineWidth: 1.5))
+                        })
+                        
+                    }
+                    .padding(.top, -25)
+                    .padding(.bottom, -10)
+
+                }
             }
         }
     }
@@ -69,6 +100,17 @@ struct UserProfile: View {
         let progress = 20 / titleOffset
         let offset = 60 * (progress > 0 && progress <= 1 ? progress : 1)
         return offset
+    }
+    
+    func getOffset() -> CGFloat {
+        let progress = (-offset / 80) * 20
+        return progress <= 20 ? progress : 20
+    }
+    
+    func getScale() -> CGFloat {
+        let progress = -offset / 80
+        let scale = 1.8 - (progress < 1.0 ? progress : 1)
+        return scale < 1 ? scale : 1
     }
 }
 
